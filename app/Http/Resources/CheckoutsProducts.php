1ade2c68ffd37d0d32f179ea9fb7a67da03ea788
danielfalcaodf <danielfalcao.df@gmail.com>
2021-09-07 19:59:19 -0300

    fix/number_format

--- app/Http/Resources/CheckoutsProducts.php
+++ app/Http/Resources/CheckoutsProducts.php
@@ -19,7 +19,7 @@ class CheckoutsProducts extends JsonResource
             'idcheck' => $this->idcheck,
             'idprod' => $this->idprod,
             'quant' => $this->quant,
-            'subtotal' => number_format($this->subtotal),
+            'subtotal' => number_format($this->subtotal, 2),
             'product' => new Product($this->product),
             'created_at' => $this->created_at->format('d/m/Y'),
             'updated_at' => $this->updated_at->format('d/m/Y'),
--- database/seeds/CheckoutsProductsTableSeeder.php
+++ database/seeds/CheckoutsProductsTableSeeder.php
@@ -21,17 +21,18 @@ class CheckoutsProductsTableSeeder extends Seeder
                 $idprod = $faker->numberBetween(1, 20);
                 $quant = $faker->numberBetween(1, 10);
                 $product =  DB::table('products')->find($idprod);
+                $subtotal =  number_format(($product ? ($quant *  $product->value) : 0), 2);
                 DB::table('checkouts_products')->insert([
 
                     'idcheck' =>  $idcheck,
                     'idprod' =>  $idprod,
                     'quant' => $quant,
-                    'subtotal' => ($product ? ($quant *  $product->value) : 0),
+                    'subtotal' => $subtotal,
                     'created_at' => Carbon::now(),
                     'updated_at' => Carbon::now(),
                 ]);
                 $checkouts =  DB::table('checkouts')->find($idcheck);
-                DB::table('checkouts')->where('id', $checkouts->id)->update(['value_total' => ($checkouts->value_total += ($product ? ($quant *  $product->value) : 0))]);
+                DB::table('checkouts')->where('id', $checkouts->id)->update(['value_total' => ($checkouts->value_total += $subtotal)]);
             }
         }
     }
